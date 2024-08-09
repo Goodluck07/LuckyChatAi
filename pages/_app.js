6f32eb3aeb50c68fb7722adbd2df0c15e8dbbd4a
Goodluck <badewolegoodluck55@gmail.com>
2024-08-09 13:23:49 -0500

    styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import "@/styles/globals.css";
+import './chat.module.css';
+import './auth.module.css';
+import '../components/customModal.module.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
